b2e071a96f43e1d09c297cab9dc782a6c52fc1b0
Obimbo07 <obimboausts@gmail.com>
2023-09-23 02:08:57 +0300

    api-connection

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Calculator from './components/Calc';
+import Quote from './components/Quote';
 
 function App() {
   return (
-    <Calculator />
+    <>
+      <Calculator />
+      <Quote />
+    </>
   );
 }
 
