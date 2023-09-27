df05f486914d39955702ce52920f1239b849e7e5
Obimbo07 <obimboausts@gmail.com>
2023-09-27 15:23:55 +0300

    full website design and routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
 import Calculator from './components/Calc';
 import Quote from './components/Quote';
+import Error404 from './components/Error';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <>
-      <Calculator />
-      <Quote />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="calculator" element={<Calculator />} />
+        <Route path="quote" element={<Quote />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+
     </>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to my page</h1>
+      <div className="home-banner">
+        <p className="p1">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi cumque
+          magni placeat repellendus dolorem, enim nesciunt incidunt distinctio sunt
+          corporis sed, quo, laborum eaque nam exercitationem ducimus nisi quas
+          saepe!
+        </p>
+
+        <p className="p2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam dolore
+          provident sequi, dolorum repellendus culpa incidunt dicta nobis vero,
+          debitis non excepturi voluptas? Exercitationem alias, vel esse rerum
+          facere libero.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
